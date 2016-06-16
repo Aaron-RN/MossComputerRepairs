594f6cf5a9070fbb26a7d80fe2c395d537404e48
Anthony Gibson <anthonyjrgibson@gmail.com>
2016-06-16 13:56:35 -0400

    Initial Upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+    <title>Moss Computer Repair Solutions Nassau Bahamas Cell Phone, Laptops Electronics Repairs</title>
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!--Slideshow-->
+    <link type="text/css" rel="stylesheet" href="css/lightslider.css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/lightslider.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#image-gallery').lightSlider({
+                gallery:true,
+                item:1,
+                thumbItem:9,
+                slideMargin: 0,
+                speed:1000,
+                pause: 3000,
+                auto:true,
+                loop:true,
+                pauseOnHover: true,
+                pager: true,
+                controls: true,
+                onSliderLoad: function() {
+                    $('#image-gallery').removeClass('cS-hidden');
+                }
+            });
+        });
+    </script>
+</head>
+<body>
+    <?php
+        include "components/topSect.inc";
+        include "components/serviceSect.inc";
+        include "components/cases.inc";
+    ?>
+    
+</body>
+</html>
